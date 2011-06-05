4d38369d5478a0b27dd6029bb6640b773707daf0
Aleksandr Priymak <aleksandr.priymak@gmail.com>
2011-06-06 00:30:07 +0400

    Added the first version of query storage

--- exam.py
+++ exam.py
@@ -8,15 +8,23 @@ def is_equal(user_result, good_result):
 def get_select_results(cursor, query):
   cursor.execute(query)
   return cursor.fetchall()
+
+user_query = "select * from messages where user_from = (select id from people where name = 'Alice');"
+
+good_queries = [
+  "select * from messages where user_from = '1'",
+  "select distinct p.name from people p, messages m\
+   where (p.id = m.user_from) or (p.id = m.user_to)",
+  "select select p.name, COUNT(p.name) from people p, messages m\
+  where (p.id = m.user_from)\
+  group by p.name;"
+];
   
 def main():
-  user_query = "select * from people;"
-  good_query = "select * from people where sex = 'w';"
-
   database = sqlite3.connect("/tmp/s1")
   cursor = database.cursor()
   user_result = get_select_results(cursor, user_query);
-  good_result = get_select_results(cursor, good_query);
+  good_result = get_select_results(cursor, good_queries[0]);
 
   print is_equal(user_result, good_result)
 
